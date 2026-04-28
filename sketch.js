72f1668d3171847150df4787df248dead75a9dc6
Jheartit <jhvely55@gmail.com>
2026-04-28 13:57:42 +0900

    2

--- sketch.js
+++ sketch.js
@@ -4,3 +4,14 @@ let dots = [], pac, ghosts = [];
 let score = 0, energy = 3;
 let state = 'PLAY'; 
 let restartT = 0, flashT = 0;
+
+// 벽 판별
+function isWall(c, r) {
+    if (c<0 || c >= COLS || r<0 || r >= ROWS) return true;
+    if ( c === 0 || c === COLS-1 {
+        if ( r ===10 || r === 13) return false;
+        return true;
+    }
+    if (!mapPx) return false;
+    
+}

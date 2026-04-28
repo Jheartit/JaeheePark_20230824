bcc5e24c5f943fc5eea72d3950922b3e0b57fe6d
Jheartit <jhvely55@gmail.com>
2026-04-28 14:09:57 +0900

    5

--- sketch.js
+++ sketch.js
@@ -28,5 +28,18 @@ function makePac() {
 }
 
 function updatePac(p) {
-    
+    let cx = p.col * TILE + TILE/2, cy = p.row * TILE + TILE/2;
+    if (abs(p.x - cx) < 3 && abs(p.y - cy) < 3) {
+        if (!isWall(p.col+p.ndx, p.row+p.ndy)) { p.dx = p.ndx; p.dy = p.ndy; }
+        if (isWall(p.col+p.dx, p.row+p.dy)) { p.dx = 0; p.dy = 0; }
+        p.x=cx; p.y=cy;
+    }
+    p.x += p.dx * 2; p.y += p.dy * 2;
+    p.col = floor(p.x / TILE); p.row = floor(p.y / TILE);
+    if (p.x<0){p.x=COLS*TILE-1; p.col=COLS-1;}
+    if (p.x>=COLS*TILE){p.x=1; p.col=0;}
+
+    if (p.dx || p.dy){ p.mouth+=0.05*p.md; if(p.mouth>0.35)p.md=-1; if(p.mouth<0.02)p.md=1;}
+
+    // 콩 먹기
 }
